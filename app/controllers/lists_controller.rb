45a009cd641f81e68c19d82d6dea95f86567d1a0
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-08-03 23:03:57 +0000

    [Finish] 3-11

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,13 +5,16 @@ class ListsController < ApplicationController
   end
 
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    # 1. データを受け取り新規登録するためのインスタンス作成
     @list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
+    # 2. データをデータベースに保存するためのsaveメソッド実行
     if @list.save
-    # 詳細画面へリダイレクト
-    redirect_to list_path(@list.id)
+      #3. フラッシュメッセージを定義し、詳細画面へリダイレクト
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to list_path(@list.id)
     else
+      #4. フラッシュメッセージを定義し、new.html.erbを描画する
+      flash.now[:alert] = "投稿に失敗しました。"
       render :new
     end
   end
