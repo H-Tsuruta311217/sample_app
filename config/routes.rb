53eece41ee6ce7854ad7f125f26a12ecfaa4500b
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-08-03 03:38:24 +0000

    [Finish] 3-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists/edit'
+
   get '/top' => 'homes#top'
-  get 'lists' => 'lists#index'
-  # .../lists/1 や .../lists/3 に該当する
-  get 'lists/:id' => 'lists#show', as: 'list'
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
